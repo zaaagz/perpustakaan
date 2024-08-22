766416aa8bdd35d3ba7ef06e6189db6ad8648631
zaaagz <gazarohmat8@gmail.com>
2024-08-22 09:48:04 +0700

    first commit

---
+++ home.php
@@ -0,0 +1,60 @@
+
+<h1 class="mt-4">Dashboard</h1>
+                        <ol class="breadcrumb mb-4">
+                            <li class="breadcrumb-item active">Dashboard</li>
+                        </ol>
+                        <div class="row">
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-primary text-white mb-4">
+                                    <div class="card-body">
+                                        <?php
+                                            echo mysqli_num_rows(mysqli_query($koneksi, "SELECT*FROM kategori"));
+                                        ?>
+                                    total kategori</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="#">View Details</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-warning text-white mb-4">
+                                    <div class="card-body">
+                                    <?php
+                                            echo mysqli_num_rows(mysqli_query($koneksi, "SELECT*FROM buku"));
+                                        ?>
+                                        total buku</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="#">View Details</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-success text-white mb-4">
+                                    <div class="card-body">
+                                    <?php
+                                            echo mysqli_num_rows(mysqli_query($koneksi, "SELECT*FROM ulasan"));
+                                        ?>
+                                        total ulasan</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="#">View Details</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-danger text-white mb-4">
+                                    <div class="card-body">
+                                    <?php
+                                            echo mysqli_num_rows(mysqli_query($koneksi, "SELECT*FROM user"));
+                                        ?>
+                                        total user</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="#">View Details</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
